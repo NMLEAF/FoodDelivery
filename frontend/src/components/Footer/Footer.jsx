461245f40b517e8a8f88d8a624292bd4ee1668da
NMLEAF <nathanmes124@gmail.com>
2024-03-31 19:52:43 +0300

    First commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+            temporibus doloribus totam, quod veniam expedita similique
+            exercitationem hic recusandae nemo aspernatur provident quaerat
+            minima aliquid soluta ab sunt maiores. Voluptatibus!
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+123-234123</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 &copy; Tomato.com - All Right Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
